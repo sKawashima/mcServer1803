f2fcf4236800baf457f9f5d551fa4294cc5e7a0b
Shota Kawashima <bysk.333@gmail.com>
2018-03-21 03:59:17 +0900

    make about

--- pages/about.vue
+++ pages/about.vue
@@ -2,9 +2,12 @@
 section.container
   div
     h1 ABOUT
-    p 2018年3月20日、sKは自宅PCのOSをManjaroに乗り換えた。
-    p せっかくLinux系に乗り換えたので、サーバー関係を調べてみた。
-    p で、以前名前だけ聞いてろくに学んでなかった<b>nginx</b>に再開した。
+    p 2018年3月20日、sKは自宅PCのOSを<b>Manjaro</b>に乗り換えた。
+       | せっかくLinux系に乗り換えたので、サーバー関係を調べてみた。
+       | 以前名前だけ聞いてろくに学んでなかった<b>nginx</b>に再会した。
+       | で、ほぼ同時にローカルサーバーをグローバルにする<b>ngrok</b>と出会った。
+       | 気づいたら、割りと時間をかけてMicecraftのサーバーを建てていた。
+       | 今は割と時間をかけてこのWebサイトを作っている。なんだこれ。
 </template>
 
 <script>
@@ -13,4 +16,6 @@ export default {
 </script>
 
 <style lang='sass' scoped>
+  p
+    max-width: 750px
 </style>
